9ce5523420ca805a4553b3d0b5df4722a00f65c3
Michellosky <125925489+MISIIc@users.noreply.github.com>
2024-02-23 18:15:27 +0100

    Update inquinambotpy.py

--- inquinambotpy.py
+++ inquinambotpy.py
@@ -11,8 +11,6 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Hai fatto l\'accesso come {bot.user}')
-    #await send("Il bot è acceso, scrivi lista per avere la lista dei comandi")
-
 
 @bot.command()
 async def lista(ctx):
